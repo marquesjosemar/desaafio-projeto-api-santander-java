b2ef7a9d491c6bcf20069637ada57716fb7cc605
marquesjosemar <126707247+marquesjosemar@users.noreply.github.com>
2024-06-20 17:02:15 -0300

    first commit

---
+++ src/test/java/me/dio/santander_backend_java_2024/SantanderBackendJava2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.santander_backend_java_2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderBackendJava2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
